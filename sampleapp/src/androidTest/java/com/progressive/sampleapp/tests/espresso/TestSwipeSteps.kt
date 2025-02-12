5f151d13296cd77016a32f82e59646a784ae032c
smugleafdev <smugleafdev@gmail.com>
2025-02-12 11:44:17 -0700

    Add some stability to swipe tests

--- sampleapp/src/androidTest/java/com/progressive/sampleapp/tests/espresso/TestSwipeSteps.kt
+++ sampleapp/src/androidTest/java/com/progressive/sampleapp/tests/espresso/TestSwipeSteps.kt
@@ -1,6 +1,9 @@
 package com.progressive.sampleapp.tests.espresso
 
-import com.progressive.kherkin.sampleapp.R
+import com.progressive.kherkin.common.testcore.And
+import com.progressive.kherkin.common.testcore.Given
+import com.progressive.kherkin.common.testcore.Then
+import com.progressive.kherkin.common.testcore.When
 import com.progressive.kherkin.espresso.steps.actions.ISwipeDown
 import com.progressive.kherkin.espresso.steps.actions.ISwipeDownOn
 import com.progressive.kherkin.espresso.steps.actions.ISwipeDownOnView
@@ -15,17 +18,13 @@ import com.progressive.kherkin.espresso.steps.actions.ISwipeUpOn
 import com.progressive.kherkin.espresso.steps.actions.ISwipeUpOnView
 import com.progressive.kherkin.espresso.steps.actions.ISwipeUpUntilISee
 import com.progressive.kherkin.espresso.steps.actions.ISwipeUpUntilISeeTheView
-import com.progressive.kherkin.espresso.steps.assertion.IShouldNotSeeText
-import com.progressive.kherkin.espresso.steps.assertion.IShouldSeeText
+import com.progressive.kherkin.espresso.steps.actions.IWaitToSeeTextOnCurrentScreen
 import com.progressive.kherkin.espresso.steps.assertion.IShouldSeeTextIsCompletelyDisplayed
 import com.progressive.kherkin.espresso.steps.assertion.IShouldSeeTextIsNotCompletelyDisplayed
 import com.progressive.kherkin.espresso.steps.assertion.IShouldSeeViewIsCompletelyDisplayed
 import com.progressive.kherkin.espresso.steps.assertion.IShouldSeeViewIsNotCompletelyDisplayed
 import com.progressive.kherkin.espresso.steps.setup.IRenderScreen
-import com.progressive.kherkin.common.testcore.And
-import com.progressive.kherkin.common.testcore.Given
-import com.progressive.kherkin.common.testcore.Then
-import com.progressive.kherkin.common.testcore.When
+import com.progressive.kherkin.sampleapp.R
 import com.progressive.sampleapp.screens.espresso.FinalScreen
 import com.progressive.sampleapp.screens.espresso.ViewPagerScreen
 import com.progressive.sampleapp.setup.SampleBaseIntegrationTestCase
@@ -35,23 +34,35 @@ import org.junit.Test
 class TestSwipeSteps : SampleBaseIntegrationTestCase() {
 
     @Test
-    fun testSwipeLeftScrollsRight() {
+    fun testSwipeLeftOnTextScrollsRight() {
         Given.IRenderScreen(ViewPagerScreen())
         And.IShouldSeeTextIsCompletelyDisplayed("3")
-        And.ISwipeLeftOn("3")
-        When.ISwipeLeftOnView(R.id.pager)
-        Then.IShouldSeeText("5")
-        And.IShouldNotSeeText("3")
+        When.ISwipeLeftOn("3")
+        Then.IWaitToSeeTextOnCurrentScreen("4")
     }
 
     @Test
-    fun testSwipeRightScrollsLeft() {
+    fun testSwipeLeftOnViewScrollsRight() {
         Given.IRenderScreen(ViewPagerScreen())
         And.IShouldSeeTextIsCompletelyDisplayed("3")
-        And.ISwipeRightOn("3")
+        When.ISwipeLeftOnView(R.id.pager)
+        Then.IWaitToSeeTextOnCurrentScreen("4")
+    }
+
+    @Test
+    fun testSwipeRightOnTextScrollsLeft() {
+        Given.IRenderScreen(ViewPagerScreen())
+        And.IShouldSeeTextIsCompletelyDisplayed("3")
+        When.ISwipeRightOn("3")
+        Then.IWaitToSeeTextOnCurrentScreen("2")
+    }
+
+    @Test
+    fun testSwipeRightOnViewScrollsLeft() {
+        Given.IRenderScreen(ViewPagerScreen())
+        And.IShouldSeeTextIsCompletelyDisplayed("3")
         When.ISwipeRightOnView(R.id.pager)
-        Then.IShouldSeeText("1")
-        And.IShouldNotSeeText("3")
+        Then.IWaitToSeeTextOnCurrentScreen("2")
     }
 
     @Test
@@ -60,7 +71,7 @@ class TestSwipeSteps : SampleBaseIntegrationTestCase() {
         And.ISwipeLeft()
         And.IShouldSeeTextIsCompletelyDisplayed("4")
         When.ISwipeRight()
-        Then.IShouldSeeText("3")
+        Then.IShouldSeeTextIsCompletelyDisplayed("3")
     }
 
     @Test
