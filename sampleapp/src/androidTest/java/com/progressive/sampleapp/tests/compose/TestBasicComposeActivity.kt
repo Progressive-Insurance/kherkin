9a4349305d3d307215c5b62aa07d851734fb1546
Paris Rouse <prsrouse@gmail.com>
2025-05-08 19:23:35 +0000

    Updated files from fork

--- sampleapp/src/androidTest/java/com/progressive/sampleapp/tests/compose/TestBasicComposeActivity.kt
+++ sampleapp/src/androidTest/java/com/progressive/sampleapp/tests/compose/TestBasicComposeActivity.kt
@@ -1,28 +1,23 @@
 package com.progressive.sampleapp.tests.compose
 
-import androidx.compose.ui.test.junit4.createEmptyComposeRule
+import com.progressive.kherkin.common.testcore.And
+import com.progressive.kherkin.common.testcore.Given
+import com.progressive.kherkin.common.testcore.Then
+import com.progressive.kherkin.common.testcore.When
 import com.progressive.kherkin.compose.steps.actions.ITouchText
 import com.progressive.kherkin.compose.steps.actions.IWaitToSeeScreen
 import com.progressive.kherkin.compose.steps.assertion.IShouldSeeText
 import com.progressive.kherkin.compose.steps.assertion.IShouldSeeTextIsClickable
 import com.progressive.kherkin.compose.steps.setup.IRenderScreen
-import com.progressive.kherkin.sampleapp.R
 import com.progressive.kherkin.espresso.steps.actions.ITouchButton
 import com.progressive.kherkin.espresso.steps.setup.IRenderScreen
-import com.progressive.kherkin.common.testcore.And
-import com.progressive.kherkin.common.testcore.Given
-import com.progressive.kherkin.common.testcore.Then
-import com.progressive.kherkin.common.testcore.When
+import com.progressive.kherkin.sampleapp.R
 import com.progressive.sampleapp.screens.compose.BasicComposeScreen
 import com.progressive.sampleapp.screens.espresso.MainScreen
 import com.progressive.sampleapp.setup.SampleBaseIntegrationTestCase
-import org.junit.Rule
 import org.junit.Test
 
 class TestBasicComposeActivity : SampleBaseIntegrationTestCase() {
-
-    @get:Rule
-    val composeTestRule = createEmptyComposeRule()
 
     @Test
     fun testHybridTestCanNavigateFromXmlScreenToComposeScreen() {
