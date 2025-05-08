c26adc2ead75db37f59cb0c05bbe5e30f63a6a36
Paris Rouse <prsrouse@gmail.com>
2025-05-08 14:57:56 -0400

    Update SampleBaseIntegrationTestCase.kt

--- sampleapp/src/androidTest/java/com/progressive/sampleapp/setup/SampleBaseIntegrationTestCase.kt
+++ sampleapp/src/androidTest/java/com/progressive/sampleapp/setup/SampleBaseIntegrationTestCase.kt
@@ -1,14 +1,19 @@
 package com.progressive.sampleapp.setup
 
 import android.content.Context
+import androidx.compose.ui.test.junit4.createEmptyComposeRule
 import androidx.test.core.app.ApplicationProvider
 import com.progressive.kherkin.common.injection.BaseIntegrationComponentHolder
 import com.progressive.kherkin.common.testcore.BaseIntegrationTestCase
 import com.progressive.kherkin.common.testcore.IntegrationTestCleanup
 import org.junit.After
 import org.junit.Before
+import org.junit.Rule
 
 open class SampleBaseIntegrationTestCase : BaseIntegrationTestCase() {
+
+    @get:Rule
+    val composeTestRule = createEmptyComposeRule()
 
     @Before
     override fun setUp() {
