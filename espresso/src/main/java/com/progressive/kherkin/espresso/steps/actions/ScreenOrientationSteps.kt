84cf55c31c68c5822d7a91638693b34bc7f9f26b
smugleafdev <smugleafdev@gmail.com>
2025-08-29 13:05:35 -0600

    Add comments

--- espresso/src/main/java/com/progressive/kherkin/espresso/steps/actions/ScreenOrientationSteps.kt
+++ espresso/src/main/java/com/progressive/kherkin/espresso/steps/actions/ScreenOrientationSteps.kt
@@ -5,10 +5,17 @@ import androidx.test.espresso.device.EspressoDevice.Companion.onDevice
 import androidx.test.espresso.device.action.ScreenOrientation
 import com.progressive.kherkin.common.testcore.Gherkin
 
+/**
+ * Rotates the emulator or device to landscape orientation.
+ * Remember to rotate back to portrait, or use a rule to start tests in the desired orientation.
+ */
 fun Gherkin.IRotateToLandscape() {
     onDevice().setScreenOrientation(ScreenOrientation.LANDSCAPE)
 }
 
+/**
+ * Rotates the emulator or device to portrait orientation.
+ */
 fun Gherkin.IRotateToPortrait() {
     onDevice().setScreenOrientation(ScreenOrientation.PORTRAIT)
 }
